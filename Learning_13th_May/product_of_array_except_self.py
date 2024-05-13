75a40e969a2c2401466e2227fc208453346265e2
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-05-13 12:47:28 +0530

    upadted

---
+++ Learning_13th_May/product_of_array_except_self.py
@@ -0,0 +1,29 @@
+# class Solution(object):
+#     def productExceptSelf(self, nums):
+#         prefix = 1
+#         suffix = 1
+#         final = [1]*len(nums)
+#         for i in range(len(nums)):
+#             final[i] = prefix
+#             prefix = prefix * nums[i]
+#         for i in range(len(nums)-1,-1,-1):
+#             final[i] = final[i]*suffix
+#             suffix = suffix * nums[i]
+#         return final
+
+import copy
+nums = [1,2,3,4]
+final = copy.deepcopy(nums)
+
+for i in range(len(nums)):
+    index = i
+    sorted = copy.deepcopy(nums)
+    sorted.pop(i)
+    count = 1
+    for j in sorted:
+        count = count*j
+    final[i] = count
+return final
+
+
+
