c235736d85515a6826a797f258d49161b4709072
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-05-30 18:02:00 +0530

    Updated 29th May

--- Learning_28th_May/my_project/myapp/urls.py
+++ Learning_28th_May/my_project/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),    #---main/root url path---- Here index means function
     path('counter', views.counter, name='counter')
 ]
--- Learning_28th_May/my_project/myapp/views.py
+++ Learning_28th_May/my_project/myapp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def counter(request):
-    # getting the data using get method using textarea name
-    text = request.GET['text']
-    words = len(text.split())
-    return render(request, 'counter.html', {'words': words})
+    words = request.POST['count']
+    text_length = len(words.split())
+    return render(request, 'counter.html', {'length': text_length})
+
 
 # When a request matches the root URL of the project (i.e., http://127.0.0.1:8000/), Django first checks the project-level urlpatterns and sees that it includes the myapp/urls.py for this URL. It then checks the app-level urlpatterns in myapp/urls.py, finds the pattern path('', views.home, name='home'), and calls the home view function from myapp/views.py.
