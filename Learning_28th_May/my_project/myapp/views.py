8ec3a9e3dc5f1b39671db8bf8bd5f327fc1ab82d
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-06-04 18:04:09 +0530

    Update 4th June

--- Learning_28th_May/my_project/myapp/views.py
+++ Learning_28th_May/my_project/myapp/views.py
@@ -9,27 +9,27 @@ def index(request):
     feature1.id = 1
     feature1.name = 'Naresh'
     feature1.details = 'Devops Engineer'
-    
+    feature1.is_true = True    
 
     feature2 = Feature()
     feature2.id = 2
     feature2.name = 'Vijay'
     feature2.details = 'UI/UX Designer'
+    feature2.is_true = True
 
     feature3 = Feature()
     feature3.id = 3
     feature3.name = 'Mohan'
     feature3.details = 'Software Developer'
+    feature3.is_true = False
 
     features = [feature1, feature2, feature3]
 
     return render(request,'index.html', {'features': features})
 
-
 def counter(request):
     words = request.POST['count']
     text_length = len(words.split())
     return render(request, 'counter.html', {'length': text_length})
-
 
 # When a request matches the root URL of the project (i.e., http://127.0.0.1:8000/), Django first checks the project-level urlpatterns and sees that it includes the myapp/urls.py for this URL. It then checks the app-level urlpatterns in myapp/urls.py, finds the pattern path('', views.home, name='home'), and calls the home view function from myapp/views.py.
