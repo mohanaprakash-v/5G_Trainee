69f96eb39bd54a211f838f867e1f72199e1dc423
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-06-05 18:02:34 +0530

    Update on June 5th 2024

--- Learning_28th_May/my_project/myapp/views.py
+++ Learning_28th_May/my_project/myapp/views.py
@@ -5,25 +5,7 @@ from .models import Feature
 # Create your views here.
 def index(request):
 
-    feature1 = Feature()
-    feature1.id = 1
-    feature1.name = 'Naresh'
-    feature1.details = 'Devops Engineer'
-    feature1.is_true = True    
-
-    feature2 = Feature()
-    feature2.id = 2
-    feature2.name = 'Vijay'
-    feature2.details = 'UI/UX Designer'
-    feature2.is_true = True
-
-    feature3 = Feature()
-    feature3.id = 3
-    feature3.name = 'Mohan'
-    feature3.details = 'Software Developer'
-    feature3.is_true = False
-
-    features = [feature1, feature2, feature3]
+    features = Feature.objects.all()
 
     return render(request,'index.html', {'features': features})
 
