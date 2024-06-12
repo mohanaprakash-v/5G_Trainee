bc82cd7dbb2422d28ffadf218d2a670385977567
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-06-12 18:16:34 +0530

    Updated on 12th June

--- Learning_28th_May/my_project/myapp/urls.py
+++ Learning_28th_May/my_project/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),    #---main/root url path---- Here index means function
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register')
 ]
--- Learning_28th_May/my_project/myapp/views.py
+++ Learning_28th_May/my_project/myapp/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-
     features = Feature.objects.all()
-
     return render(request,'index.html', {'features': features})
 
 def counter(request):
     words = request.POST['count']
     text_length = len(words.split())
     return render(request, 'counter.html', {'length': text_length})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            pass
+
+    return render(request, 'register.html')
 
 # When a request matches the root URL of the project (i.e., http://127.0.0.1:8000/), Django first checks the project-level urlpatterns and sees that it includes the myapp/urls.py for this URL. It then checks the app-level urlpatterns in myapp/urls.py, finds the pattern path('', views.home, name='home'), and calls the home view function from myapp/views.py.
