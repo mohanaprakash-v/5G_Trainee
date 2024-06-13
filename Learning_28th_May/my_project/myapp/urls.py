0a8a0b893cddee42b9d09ed0305968e3b05071cd
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-06-13 18:27:27 +0530

    June 13 Update

--- Learning_28th_May/my_project/myapp/urls.py
+++ Learning_28th_May/my_project/myapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),    #---main/root url path---- Here index means function
-    path('counter', views.counter, name='counter'),
-    path('register', views.register, name='register')
+    path('', views.index, name='home'),
+    path('counter/', views.counter, name='counter'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+
 ]
--- PostManAPI_Django_RestFrameworks/views.py
+++ PostManAPI_Django_RestFrameworks/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login  # Correct imports
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
     features = Feature.objects.all()
-    # username = None
-    # if request.user.is_authenticated:
-    #     username = request.user.username
-    return render(request, 'index.html', {'features': features})
+    return render(request,'index.html', {'features': features})
 
 def counter(request):
     words = request.POST['count']
@@ -27,10 +23,10 @@ def register(request):
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request, "Email already exists and is in use!")
+                messages.info(request, "Email Already Exists and Used!")
                 return redirect('register')
             elif User.objects.filter(username=username).exists():
-                messages.info(request, "Username already exists!")
+                messages.info(request, "Username Already Exist!")
                 return redirect('register')
             else:
                 # creating a user
@@ -38,7 +34,7 @@ def register(request):
                 user.save()
                 return redirect('login')
         else:
-            messages.info(request, "Passwords do not match!")
+            messages.info(request, "Passwords Not Same !!")
             return redirect('register')
 
     return render(request, 'register.html')
@@ -58,3 +54,5 @@ def login(request):
             return redirect('login')
         
     return render(request, 'login.html')
+
+    
