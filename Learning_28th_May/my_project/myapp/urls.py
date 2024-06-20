068dc2023bbcc618e33813a20a06f71aae472d37
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-06-20 10:01:49 +0530

    Updated

--- Learning_28th_May/my_project/myapp/urls.py
+++ Learning_28th_May/my_project/myapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('get_csrf_token/', views.get_csrf_token, name='get_csrf_token'),
     path('profile_page', views.profile_page, name='profile_page'),
     path('profile', views.profile, name='profile'),
+    
 ]
