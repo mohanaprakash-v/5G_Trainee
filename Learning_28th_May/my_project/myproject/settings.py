5d18473f4cbbce4ab2d8fd58e6a8b5b6ae468513
MohanaPrakash V <mpsprakash2002@gmail.com>
2024-11-03 11:13:44 +0530

    GIT_Merge_Practise

--- Learning_28th_May/my_project/myproject/settings.py
+++ Learning_28th_May/my_project/myproject/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework',   
+    'rest_framework',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
